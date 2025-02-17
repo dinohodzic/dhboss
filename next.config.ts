f61e982bcfec3bfbf7a3cc0651502d58670f54e1
Dino Hodzic <hodzicdinost1@gmail.com>
2025-02-17 15:29:49 +0000

    d7

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
-    
-    const nextConfig: NextConfig = {
-      /* ignore build errors */
-      ignoreBuildErrors: true,
 
-eslint:{
-    ignoreDuringBuilds: true, 
-  }
-     
+const nextConfig: NextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
